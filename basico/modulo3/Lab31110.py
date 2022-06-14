05d48a2e789280e82e794cbd5b9a6c5ce829bcae
“elena03rangel” <1219100404@alumnos.utng.edu.mx>
2022-06-14 17:47:49 -0500

    Curso

---
+++ basico/modulo3/Lab31110.py
@@ -0,0 +1,9 @@
+#Laboratorio 3.1.1.10
+word = input('Ingresa el nombre de la palabra: ')
+
+if word == "ESPATIFILIO":
+    print("Si, ¡EL ESPATIFILIO! es la mejor planta de todos los tiempos!")
+elif word == "espatifilo":
+    print("No, ¡quiero un gran ESPATIFILIO!")
+else:
+    print("¡ESPATIFILIO!, ¡No [entrada]!")
